5430182e0dafcd9e05e08124885b164b90734eef
Silvia-Wachira <wachirasilvia8.com>
2024-04-08 22:55:06 +0300

    style the component

--- src/components/TransactionForm.js
+++ src/components/TransactionForm.js
@@ -3,25 +3,26 @@ import React, {useRef, useState} from 'react'
  // Transaction Form
 const TransactionForm = () => {
 
-    const handleSubmit = async (e) => {
+    const  handleSubmit = async (e) => {
         e.preventDefault()
-        const accountId = e.target[0].value
-        const amount = e.target[1].value
-        const url = "https://infra.devskills.app/api/accounting/transaction"
-        const dataObj = {
-          "account_id": accountId,
-          "amount": amount
-        }
-        const requestOptions = {
-          method: "POST",
-          headers: {
-           "Content-Type":"application/json"
-          },
-          body: JSON.stringify(dataObj)
-        }
-        const response = await fetch(url, requestOptions)
-        const data = await response.json()
-        e.target.reset()
+           const accountId = e.target[0].value
+           const amount = e.target[1].value
+           const url = "https://infra.devskills.app/api/accounting/transaction"
+           const dataObj = {
+             "account_id": accountId,
+             "amount": amount
+           }
+           const requestOptions = {
+             method: "POST",
+             headers: {
+              "Content-Type":"application/json"
+             },
+             body: JSON.stringify(dataObj)
+           }
+           const response = await fetch(url, requestOptions)
+           const data = await response.json()
+           e.target.reset()
+          // console.log("logged")
     }
 
   return (
@@ -31,6 +32,7 @@ const TransactionForm = () => {
             <input  className='border rounded p-1' placeholder='Account ID' data-type="account-id"/>
             <input  className='border rounded p-1' placeholder='Amount' data-type="amount"/>
             <input className='bg-green-500 text-white rounded p-1' data-type="transaction-submit" type="submit"/>
+            {/* <button type='submit'>Submit</button> */}
         </form>
 
     </div>
